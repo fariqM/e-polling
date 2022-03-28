dc8289df064aa3e12c55c5c4dd56eb7d5e245116
fariqM <maulana.fariq30@gmail.com>
2022-03-28 15:35:51 +0700

    update

---
+++ dist/js/about.68afce46.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunke_polling"]=self["webpackChunke_polling"]||[]).push([[443],{9030:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var l=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},u=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"about"},[t("h1",[e._v("This is an about page")])])}],s=t(1001),a={},r=(0,s.Z)(a,l,u,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.68afce46.js.map
