69303cdac5689c094d2c1cf40cff296b8b414193
fariqM <maulana.fariq30@gmail.com>
2022-03-28 16:29:18 +0700

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,7 +13,20 @@
 			<v-col class="mb-5" cols="12">
 				<div class="d-flex justify-space-around align-center">
 					<v-btn :to="{ name: 'maps' }">Go to Map Testing</v-btn>
-					<v-btn :to="{ name: 'polling', params: { pollingUrl: 'kjD7123S' } }" color="info">Go to Polling</v-btn>
+				</div>
+			</v-col>
+			<v-col class="" cols="12">
+				<div class="">
+					<v-text-field
+						placeholder="Insert a link or polling code"
+						label="Polling Address"
+						v-model="pol_address"
+					></v-text-field>
+					<v-btn
+						:to="{ name: 'polling', params: { pollingUrl: pol_address } }"
+						color="info"
+						>Go to Polling</v-btn
+					>
 				</div>
 			</v-col>
 		</v-row>
@@ -25,56 +38,7 @@ export default {
 	name: "HelloWorld",
 
 	data: () => ({
-		ecosystem: [
-			{
-				text: "vuetify-loader",
-				href: "https://github.com/vuetifyjs/vuetify-loader",
-			},
-			{
-				text: "github",
-				href: "https://github.com/vuetifyjs/vuetify",
-			},
-			{
-				text: "awesome-vuetify",
-				href: "https://github.com/vuetifyjs/awesome-vuetify",
-			},
-		],
-		importantLinks: [
-			{
-				text: "Documentation",
-				href: "https://vuetifyjs.com",
-			},
-			{
-				text: "Chat",
-				href: "https://community.vuetifyjs.com",
-			},
-			{
-				text: "Made with Vuetify",
-				href: "https://madewithvuejs.com/vuetify",
-			},
-			{
-				text: "Twitter",
-				href: "https://twitter.com/vuetifyjs",
-			},
-			{
-				text: "Articles",
-				href: "https://medium.com/vuetify",
-			},
-		],
-		whatsNext: [
-			{
-				text: "Explore components",
-				href: "https://vuetifyjs.com/components/api-explorer",
-			},
-			{
-				text: "Select a layout",
-				href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
-			},
-			{
-				text: "Frequently Asked Questions",
-				href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-			},
-		],
-	}),
+    pol_address:"",
+  }),
 };
 </script>
